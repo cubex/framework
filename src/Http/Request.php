154b7e6774f551337e16892d1656ab57b070eaea
Brooke Bryan <brooke@bajb.net>
2015-04-24 09:41:45 +0100

    Follow X-Forwarded for rules, to pick the correct client IP

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -243,7 +243,7 @@ class Request extends \Symfony\Component\HttpFoundation\Request
   /**
    * Retrieve a section of the path
    *
-   * @param int $offset
+   * @param int  $offset
    * @param null $limit
    *
    * @return string
@@ -316,5 +316,32 @@ class Request extends \Symfony\Component\HttpFoundation\Request
       'REFERER',
       $this->server->get('HTTP_REFERER', $default)
     );
+  }
+
+  /**
+   * Returns the client IP address.
+   *
+   * This method can read the client IP address from the "X-Forwarded-For"
+   * header when trusted proxies were set via "setTrustedProxies()". The
+   * "X-Forwarded-For" header value is a comma+space separated list of IP
+   * addresses, the left-most being the original client, and each successive
+   * proxy that passed the request adding the IP address where it received the
+   * request from.
+   *
+   * If your reverse proxy uses a different header name than "X-Forwarded-For",
+   * ("Client-Ip" for instance), configure it via "setTrustedHeaderName()" with
+   * the "client-ip" key.
+   *
+   * @return string The client IP address
+   *
+   * @see getClientIps()
+   * @see http://en.wikipedia.org/wiki/X-Forwarded-For
+   *
+   * @api
+   */
+  public function getClientIp()
+  {
+    $ipAddresses = $this->getClientIps();
+    return end($ipAddresses);
   }
 }
