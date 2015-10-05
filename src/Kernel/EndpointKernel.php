62e5f0724dcabb3714146b86348ed79c1fcc02ae
Dave Blake <me@daveblake.co.uk>
2015-10-05 14:26:34 +0100

    [ApiResponse] Allow ApiResponse to send custom HTTP status

--- src/Kernel/EndpointKernel.php
+++ src/Kernel/EndpointKernel.php
@@ -19,14 +19,19 @@ abstract class EndpointKernel extends CubexKernel
   {
     if($value instanceof ApiResponseInterface)
     {
+
+      $statusCode =
+        method_exists($value, 'getStatusCode') ? $value->getStatusCode() : 200;
+
       $format = new JsonFormat();
-      $value = Response::create(
+      $value  = Response::create(
         $format->encode(
           $value->toArray(),
-          200,
+          $statusCode,
           '',
           '\\' . get_class($value)
-        )
+        ),
+        $statusCode
       );
       $value->headers->set("Content-Type", "application/json");
     }
