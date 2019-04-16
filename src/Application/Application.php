f0fd4d66b080eb6a4e3052466ec3106f3ab1be98
Brooke Bryan <brooke@bajb.net>
2019-04-16 14:45:52 +0100

    Application placeholder

---
+++ src/Application/Application.php
@@ -0,0 +1,12 @@
+<?php
+namespace Cubex\Application;
+
+use Cubex\Routing\ConditionSelector;
+
+/**
+ * Class Application
+ * Placeholder for future application logic
+ */
+abstract class Application extends ConditionSelector
+{
+}
