19a4a5fd8dd6b63f9c00c7a2e924b9f3c69fb6f2
Brooke Bryan <brooke@bajb.net>
2019-05-05 18:30:08 +0100

    RequestDataConstraint

---
+++ tests/Routing/RequestDataContraintTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Cubex\Tests\Routing;
+
+use Cubex\Context\Context;
+use Cubex\Routing\RequestDataContraint;
+use Packaged\Http\Request;
+use PHPUnit\Framework\TestCase;
+
+class RequestDataContraintTest extends TestCase
+{
+  protected function _makeContext()
+  {
+    return new Context(
+      Request::create(
+        'https://localhost/?query1=val1&query2=val2&query3=val3',
+        'POST',
+        ['post1' => 'val1', 'post2' => 'val2', 'post3' => 'val3'],
+        ['cookie1' => 'val1', 'cookie2' => 'val2'],
+        [],
+        ['HTTPS' => 'on']
+      )
+    );
+  }
+
+  public function testInstance()
+  {
+    $c = $this->_makeContext();
+    $i = RequestDataContraint::i();
+
+    $this->assertTrue($i->match($c));
+    $i->post('post1', 'val1');
+    $this->assertTrue($i->match($c));
+    $i->server('HTTPS');
+    $this->assertTrue($i->match($c));
+    $i->query('query2', 'val2');
+    $this->assertTrue($i->match($c));
+    $i->cookie('cookie2');
+    $this->assertTrue($i->match($c));
+    $i->cookie('cookie1', 'val1');
+    $this->assertTrue($i->match($c));
+    $i->post('missing');
+    $this->assertFalse($i->match($c));
+  }
+
+  public function testServer()
+  {
+    $this->assertTrue(RequestDataContraint::i()->server('HTTPS')->match($this->_makeContext()));
+    $this->assertTrue(RequestDataContraint::i()->server('HTTPS', 'on')->match($this->_makeContext()));
+    $this->assertFalse(RequestDataContraint::i()->server('HTTP')->match($this->_makeContext()));
+    $this->assertFalse(RequestDataContraint::i()->server('HTTPS', 'OFF')->match($this->_makeContext()));
+  }
+
+  public function testCookie()
+  {
+    $this->assertFalse(RequestDataContraint::i()->cookie('cookieN')->match($this->_makeContext()));
+    $this->assertTrue(RequestDataContraint::i()->cookie('cookie1')->match($this->_makeContext()));
+    $this->assertTrue(RequestDataContraint::i()->cookie('cookie1', 'val1')->match($this->_makeContext()));
+    $this->assertTrue(
+      RequestDataContraint::i()->cookie('cookie1', 'val1')->cookie('cookie2', 'val2')->match($this->_makeContext())
+    );
+  }
+
+  public function testPost()
+  {
+    $this->assertFalse(RequestDataContraint::i()->post('postN')->match($this->_makeContext()));
+    $this->assertTrue(RequestDataContraint::i()->post('post1')->match($this->_makeContext()));
+    $this->assertTrue(RequestDataContraint::i()->post('post1', 'val1')->match($this->_makeContext()));
+    $this->assertTrue(
+      RequestDataContraint::i()->post('post1', 'val1')->post('post2', 'val2')->match($this->_makeContext())
+    );
+  }
+
+  public function testQuery()
+  {
+    $this->assertFalse(RequestDataContraint::i()->query('queryN')->match($this->_makeContext()));
+    $this->assertTrue(RequestDataContraint::i()->query('query1')->match($this->_makeContext()));
+    $this->assertTrue(RequestDataContraint::i()->query('query1', 'val1')->match($this->_makeContext()));
+    $this->assertTrue(
+      RequestDataContraint::i()->query('query1', 'val1')->query('query2', 'val2')->match($this->_makeContext())
+    );
+  }
+}
