67be38567f3157d9fabc748829f1e6b404d75b22
Tom Kay <oridan82@gmail.com>
2019-12-11 11:25:35 +0000

    unit test default handler

--- tests/Application/ApplicationTest.php
+++ tests/Application/ApplicationTest.php
@@ -2,11 +2,12 @@
 
 namespace Cubex\Tests\Application;
 
-use Packaged\Context\Context;
 use Cubex\Cubex;
 use Cubex\Events\Handle\ResponsePrepareEvent;
 use Cubex\Tests\Supporting\Application\TestApplication;
+use Cubex\Tests\Supporting\Application\TestApplicationDefaultHandler;
 use Cubex\Tests\Supporting\Application\TestExtendedApplication;
+use Packaged\Context\Context;
 use Packaged\Http\Request;
 use PHPUnit\Framework\TestCase;
 
@@ -24,6 +25,22 @@ class ApplicationTest extends TestCase
 
     $response = $app->handle($cubex->getContext());
     $this->assertContains("App Default", $response->getContent());
+  }
+
+  public function testDefaultHandler()
+  {
+    $cubex = new Cubex(__DIR__, null, false);
+    $app = new TestApplicationDefaultHandler($cubex);
+    $response = $app->handle($cubex->getContext());
+    $this->assertEquals(404, $response->getStatusCode());
+    $this->assertContains("Not Found", $response->getContent());
+
+    $request = Request::create("/some-route");
+    $ctx = new Context($request);
+    $cubex->share(Context::class, $ctx);
+    $response = $cubex->handle($app, false);
+    $this->assertEquals(404, $response->getStatusCode());
+    $this->assertContains("Not Found", $response->getContent());
   }
 
   public function testRoute()
---
+++ tests/Supporting/Application/TestApplicationDefaultHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cubex\Tests\Supporting\Application;
+
+use Cubex\Application\Application;
+
+class TestApplicationDefaultHandler extends Application
+{
+
+}
