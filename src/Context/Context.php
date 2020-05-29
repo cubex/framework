769286b0deb6500cb38bbfcf126dc05efac12b8a
Brooke Bryan <brooke@bajb.net>
2020-05-29 09:11:22 +0100

    Support cubex-local and local-host domains

--- src/Context/Context.php
+++ src/Context/Context.php
@@ -3,11 +3,22 @@ namespace Cubex\Context;
 
 use Cubex\CubexAware;
 use Cubex\CubexAwareTrait;
+use Packaged\Http\Request;
 use Psr\Log\LoggerInterface;
 
 class Context extends \Packaged\Context\Context implements CubexAware
 {
   use CubexAwareTrait;
+
+  protected function _construct()
+  {
+    parent::_construct();
+    $r = $this->request();
+    if($r instanceof Request)
+    {
+      $r->defineTlds(['cubex-local.com', 'local-host.xyz'], true);
+    }
+  }
 
   public function log(): LoggerInterface
   {
