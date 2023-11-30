01e3fd1936bbc288c42663d75a4d7af8da46ba48
Brooke Bryan <brooke@bajb.net>
2023-11-30 15:06:29 +0000

    Add redirect routes

--- tests/Routing/RouterTest.php
+++ tests/Routing/RouterTest.php
@@ -6,6 +6,7 @@ use Cubex\Routing\Router;
 use Packaged\Context\Context;
 use Packaged\Http\Request;
 use Packaged\Http\Response;
+use Packaged\Http\Responses\RedirectResponse;
 use Packaged\Routing\Handler\FuncHandler;
 use PHPUnit\Framework\TestCase;
 
@@ -27,5 +28,21 @@ class RouterTest extends TestCase
     $handler = $router->getHandler(new Context(Request::create('/func')));
     $result = $handler->handle(new Context());
     self::assertEquals('OK', $result->getContent());
+  }
+
+  public function testRedirect()
+  {
+    $router = Router::i();
+    $handleStr = "@301!/redirected";
+    $router->onPath('/redirect', $handleStr);
+
+    $ctx = new Context(Request::create('/redirect'));
+    $handler = $router->getHandler($ctx);
+    self::assertEquals($handleStr, (string)$handler);
+
+    $response = $router->handle($ctx);
+    self::assertInstanceOf(RedirectResponse::class, $response);
+    self::assertEquals(301, $response->getStatusCode());
+    self::assertEquals('/redirected', $response->getTargetUrl());
   }
 }
